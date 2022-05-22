2ab88494b4b90d2f36a91b3130b3f6c8db4f033e
David Lee <seekdavidlee@gmail.com>
2022-05-22 17:49:34 -0500

    use first rg

--- CompleteSetup.ps1
+++ CompleteSetup.ps1
@@ -6,7 +6,7 @@ param(
 $ErrorActionPreference = "Stop"
 
 $groups = az group list --tag stack-environment=$BUILD_ENV | ConvertFrom-Json
-$resourceGroupName = ($groups | Where-Object { $_.tags.'stack-name' -eq "$STACK_NAME_TAG_PREFIX-api" -and $_.tags.'stack-environment' -eq $BUILD_ENV }).name
+$resourceGroupName = ($groups | Where-Object { $_.tags.'stack-name' -eq "$STACK_NAME_TAG_PREFIX-api" -and $_.tags.'stack-environment' -eq $BUILD_ENV })[0].name
 $aks = (az resource list -g $resourceGroupName --resource-type "Microsoft.ContainerService/managedClusters" | ConvertFrom-Json)[0]
 az aks get-credentials -n $aks.name -g $resourceGroupName --overwrite-existing
 $acr = (az resource list --tag stack-name="$STACK_NAME_TAG_PREFIX-shared-container-registry" | ConvertFrom-Json)[0]
