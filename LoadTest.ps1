c799793f2318e9c8adc07f2f82a224514156ed05
David Lee <seekdavidlee@gmail.com>
2022-06-04 13:19:22 -0500

    introduce load test

--- LoadTest.ps1
+++ LoadTest.ps1
@@ -1,18 +1,18 @@
-param([int]$Intensity = 15)
+param([int]$Intensity = 15, [int]$DayCounter = -200)
 
-$dayCounter = -400
 $url = "http://web.contoso.com"
 $success = 0
 $failure = 0
 for ($x = 0; $x -lt $Intensity; $x++) {
 
     $temp = Get-Random -Minimum -200 -Maximum 200
-    $dayCounter += 1
-    $date = (Get-Date).AddDays($dayCounter).ToString("yyyy-MM-dd") 
+    $DayCounter += 1
+    $date = (Get-Date).AddDays($DayCounter).ToString("yyyy-MM-dd") 
     $body = @{ temp = $temp; date = $date }
     
     try {
         Invoke-RestMethod -UseBasicParsing -Uri ($url + "/WeatherForecast") -Body ($body | ConvertTo-Json) -Method Put -ContentType "application/json"
+        Invoke-RestMethod -UseBasicParsing -Uri ($url + "/WeatherForecast/$date") -Method Get -ContentType "application/json"        
         $success += 1 
     }
     catch {
